ddc0e3075670ec9b89bb7cfdda947fb0fa82b755
Beck0797 <bekhzod.abduraupov@mail.ru>
2022-11-24 15:56:50 +0900

    merged two projects

---
+++ app/src/main/java/com/dndev/signupfirbase/OnFetchDataListener.java
@@ -0,0 +1,10 @@
+package com.dndev.signupfirbase;
+
+import com.dndev.signupfirbase.Models.NewsHeadlines;
+
+import java.util.List;
+
+public interface OnFetchDataListener<NewsApiResponse> {
+    void onFetchData(List<NewsHeadlines> list, String message);
+    void onError(String message);
+}
---
+++ app/src/main/java/com/dndev/signupfirbase/SelectListener.java
@@ -0,0 +1,8 @@
+package com.dndev.signupfirbase;
+
+import com.dndev.signupfirbase.Models.NewsHeadlines;
+
+public interface SelectListener {
+    void OnNewsClicked(NewsHeadlines headlines);
+
+}
